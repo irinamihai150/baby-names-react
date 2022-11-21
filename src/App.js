9fcbcec37e57c166e17d3835d59a77cfab47bbb5
irina mihai <irinamihai150@gmail.com>
2022-11-21 20:08:50 +0000

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
-
+import React, { useState } from "react";
 import BabyList from "./BabyNames";
+import SearchBar from "./SearchBar";
 
 function App() {
+   const [inputText, setInputText] = useState("");
+   let inputHandler = (e) => {
+     let lowerCase = e.target.value.toLowerCase();
+     setInputText(lowerCase);
+   };
   return (
     <div className="container">
-      <BabyList />
+      <SearchBar onChange={inputHandler} />
+      <BabyList input={inputText} />
     </div>
   );
 }
--- src/BabyNames.js
+++ src/BabyNames.js
@@ -1,22 +1,33 @@
 import React from "react";
 import names from "./BabyNamesData.js";
 
-
-function BabyList() {
+function BabyList(props) {
   let sortedNames = names.sort(function (a, b) {
-  if (a.name < b.name) {
-    return -1;
-  }
-  if (a.name > b.name) {
-    return 1;
-  }
-  return 0;
-});
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  let filteredData = sortedNames.filter((name) => {
+    if (props.input === "") {
+      return name;
+    } else {
+      return name.text.toLowerCase().includes(props.input);
+    }
+  });
   return (
     <div className="container-fluid">
       <ul>
-        {sortedNames.map(({ name, sex }) => (
-          <li style={{ backgroundColor: sex === "f" ? "#FFC6E5" : "#A0CFEE", color:sex ==="f" ? "red":"blue" }}>
+        {filteredData.map(({ name, sex }) => (
+          <li
+            style={{
+              backgroundColor: sex === "f" ? "#FFC6E5" : "#A0CFEE",
+              color: sex === "f" ? "red" : "blue",
+            }}
+          >
             {name}
           </li>
         ))}
