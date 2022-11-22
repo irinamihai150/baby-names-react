b25d02b8f492488d580205e1ac12de05844ee677
irina mihai <irinamihai150@gmail.com>
2022-11-22 20:50:30 +0000

    1

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import BabyList from "./BabyNames";
 import SearchBar from "./SearchBar";
 
 function App() {
-   const [inputText, setInputText] = useState("");
-   let inputHandler = (e) => {
-     let lowerCase = e.target.value.toLowerCase();
-     setInputText(lowerCase);
-   };
+  //  const [inputText, setInputText] = useState("");
+  //  let inputHandler = (e) => {
+  //    let lowerCase = e.target.value.toLowerCase();
+  //    setInputText(lowerCase);
+  //  };
   return (
     <div className="container">
-      <SearchBar onChange={inputHandler} />
-      <BabyList input={inputText} />
+      <SearchBar />
+      <BabyList />
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import names from "./BabyNamesData.js";
 
-export default function SearchBar() {
+const getFilteredItems = (query, items) => {
+  if (!query) {
+    return items;
+  }
+  return items.filter((el) => el.name.includes(query));
+};
+const searchBar = () => {
+  const [query, setQuery] = useState("");
+  let { names } = names;
+  let { items } = names;
+  const filteredItems = getFilteredItems(query, items);
   return (
     <div>
-      <input type="text" placeholder="Search.."></input>
+      <label> Search</label>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      <ul>
+        {filteredItems.map((value) => (
+          <li key={value.name}>{value.name}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default searchBar;
+
+// let filteredBabies = names.filter((el) => {
+//   return el.name.toLowerCase();
+// });
+// const handleChange = (name) => {
+//   name.preventDefault();
+//   setSearchInput(name.target.value);
+// };
+
+// if (searchInput.length > 0) {
+//   filteredBabies.filter((el) => {
+//     return el.name.match(searchInput);
+//   });
+// return (
+//   <div>
+//     <input
+//       type="text"
+//       placeholder="Search here"
+//       onChange={handleChange}
+//       value={searchInput}
+//     />
+//     <ul>
+//       {filteredBabies.map((baby) => {
+//         <li>{baby.index}</li>;
+//       })}
+//     </ul>
+//   </div>
+// );
+
+// const getFilteredItems = (query, items) => {
+//   if (!query) {
+//     return items;
+//   }
+//   return items.filter((el) => el.name.includes(query));
+// };
+// const searchBar = () => {
+//   const [query, setQuery] = useState("");
+//   let { names } = names;
+//   let { items } = babynames;
+//   const filteredItems = getFilteredItems(query, items);
+//   return (
+//     <div>
+//       <label> Search</label>
+//       <input type="text" onChange={(e) => setQuery(e.target.value)} />
+//       <ul>
+//         {filteredItems.map((value) => (
+//           <li key={value.name}>{value.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
